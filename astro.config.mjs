9f4aaa85e7b768e5264efc49cd7eb9e952d2a35a
MaSsoumeh Zarei <massoumehzarei@gmail.com>
2025-04-30 10:54:00 +0330

    feat: create neew card components

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,11 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from 'astro/config';
 
-import alpinejs from "@astrojs/alpinejs";
-
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
 
-  integrations: [alpinejs()],
+  integrations: [],
 });
